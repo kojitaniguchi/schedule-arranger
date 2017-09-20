2ef6e0856fc19db09aa070414e0257a3e5996139
kojitaniguchi <tanigu0511@gmail.com>
2017-09-20 13:35:00 +0900

    typo

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,7 @@
 'use strict'
 const Sequelize = require('Sequelize');
 const sequelize = new Sequelize(
-  'postgres://postgres:postgres@localhost/schedule_arranger'
+  'postgres://postgres:postgres@localhost/schedule_arranger',
   { logging: true });
 
 module.exports = {
