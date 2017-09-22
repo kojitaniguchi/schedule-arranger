9a72305fa1ff35e85fa106e9315f2d7aeb075461
kojitaniguchi <tanigu0511@gmail.com>
2017-09-22 14:04:04 +0900

    typo schedule

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,17 +1,17 @@
 'use strict'
 const express = require('express');
 const router = express.Router();
-const authenticationEnsure = require('./authentication-ensurer');
+const authenticationEnsurer = require('./authentication-ensurer');
 const uuid = require('node-uuid');
 const Schedule = require('../models/schedule');
 const Candidate = require('../models/candidate');
 const User = require('../models/user');
 
-router.get('/new', authenticationEnsure, (req, res, next) => {
+router.get('/new', authenticationEnsurer, (req, res, next) => {
   res.render('new', { user: req.user });
 });
 
-router.post('/', authenticationEnsure, (req, res, next) => {
+router.post('/', authenticationEnsurer, (req, res, next) => {
   const scheduleId = uuid.v4();
   const updatedAt = new Date();
   Schedule.create({
